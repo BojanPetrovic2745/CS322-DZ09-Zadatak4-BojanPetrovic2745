eb7036a34cd21921fc2f8b9bf54e982f2d2b833a
bojan <bojan.petrovic.2745@outlook.com>
2021-01-29 00:05:24 +0100

    Add project files.

---
+++ CS322-DZ09-Zadatak4-BojanPetrovic2745/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿
+namespace CS322_DZ09_Zadatak4_BojanPetrovic2745
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbNizDuz = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblZbir = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblSrednja = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblMin = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblMax = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(260, 88);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 84);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(375, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Unesi";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(156, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Veličina niza";
+            // 
+            // tbNizDuz
+            // 
+            this.tbNizDuz.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbNizDuz.Location = new System.Drawing.Point(260, 29);
+            this.tbNizDuz.Name = "tbNizDuz";
+            this.tbNizDuz.Size = new System.Drawing.Size(100, 22);
+            this.tbNizDuz.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(130, 211);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Zbir elemenata niza:";
+            // 
+            // lblZbir
+            // 
+            this.lblZbir.AutoSize = true;
+            this.lblZbir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblZbir.Location = new System.Drawing.Point(276, 211);
+            this.lblZbir.Name = "lblZbir";
+            this.lblZbir.Size = new System.Drawing.Size(0, 16);
+            this.lblZbir.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(130, 271);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Srednja vrednost niza:";
+            // 
+            // lblSrednja
+            // 
+            this.lblSrednja.AutoSize = true;
+            this.lblSrednja.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSrednja.Location = new System.Drawing.Point(301, 271);
+            this.lblSrednja.Name = "lblSrednja";
+            this.lblSrednja.Size = new System.Drawing.Size(0, 16);
+            this.lblSrednja.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(133, 324);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(142, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Najmanji element niza:";
+            // 
+            // lblMin
+            // 
+            this.lblMin.AutoSize = true;
+            this.lblMin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMin.Location = new System.Drawing.Point(301, 324);
+            this.lblMin.Name = "lblMin";
+            this.lblMin.Size = new System.Drawing.Size(0, 16);
+            this.lblMin.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(133, 383);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(138, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Najveći element niza: ";
+            // 
+            // lblMax
+            // 
+            this.lblMax.AutoSize = true;
+            this.lblMax.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMax.Location = new System.Drawing.Point(279, 382);
+            this.lblMax.Name = "lblMax";
+            this.lblMax.Size = new System.Drawing.Size(0, 16);
+            this.lblMax.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblMax);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblMin);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblSrednja);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblZbir);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbNizDuz);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbNizDuz;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblZbir;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblSrednja;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblMin;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblMax;
+    }
+}
+
---
+++ CS322-DZ09-Zadatak4-BojanPetrovic2745/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS322_DZ09_Zadatak4_BojanPetrovic2745
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int velicina = Convert.ToInt32(tbNizDuz.Text);
+            int sum = 0;
+            
+            int[] niz = new int[velicina];
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < velicina; i++)
+            {
+                niz[i] = rnd.Next(1, velicina);
+                listBox1.Items.Add(niz[i]);
+            }
+
+            sum = niz.Sum();
+
+            int min = niz[0];
+            
+            // suma niza
+            lblZbir.Text = sum.ToString();
+
+            // srednja vrednost niza
+            lblSrednja.Text = (sum / velicina).ToString();
+
+            for (int i = 0; i < niz.Length; i++)
+            {
+                if (niz[i] < min)
+                {
+                    min = niz[i];
+                }
+            }
+
+            lblMin.Text = min.ToString();
+
+            for (int i = 0; i < niz.Length; i++)
+            {
+                if (niz[i] > min)
+                {
+                    min = niz[i];
+                }
+                
+            }
+
+            lblMax.Text = min.ToString();
+
+
+        }
+    }
+}
